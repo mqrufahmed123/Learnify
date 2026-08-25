d0f0a4f41ce5e2731fa671e609974ac5ab2c649e
Maruf Ahmed <mqrufahmed123@gmail.com>
2026-08-26 03:14:18 +0530

    checkpoint 1.0

--- client/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-// Wraps async route handlers so thrown/rejected errors are passed to Express's
-// error middleware instead of crashing the process or needing try/catch everywhere.
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
